ed73b4d52fb18e5da22b304ebd9572528deb1ab8
RchardDayz <josedco@hotmail.com>
2023-04-06 09:20:05 -0600

    Elimine la palabra inicio de las url´s para que arranque desde cero la paguina

--- proyecto_django/urls.py
+++ proyecto_django/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('inicio/', include('inicio.urls')),
+    path('', include('inicio.urls')),
     path('admin/', admin.site.urls),
 ]
